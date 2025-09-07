7b3ecf619798d87dfed12ba09f5b18d30c1404d4
Mohamed Amine <aminogha@gmail.com>
2025-09-07 21:06:03 +0100

    feat: Implement Products and Sales Agent tables with sorting and filtering capabilities

--- dashboard_frontend/app/dashboard/tables/page.jsx
+++ dashboard_frontend/app/dashboard/tables/page.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import { OrdersTable } from "@/components/OrdersTable.jsx";
+import { ProductsTables } from "../../../components/ProductsTables";
+import { SalesAgentTable } from "../../../components/SalesAgentTable";
 
 const TablesPage = () => {
-  return <OrdersTable />;
+  return (
+    <div>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-2">
+        <ProductsTables />
+        <SalesAgentTable />
+      </div>
+      <OrdersTable />
+    </div>
+  );
 };
 
 export default TablesPage;
